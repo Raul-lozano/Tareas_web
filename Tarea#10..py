fd2a36ce6c2171689c191c51df9db9a966fabee3
Raul-lozano <86394164+Raul-lozano@users.noreply.github.com>
2021-06-30 00:02:49 -0500

    Add files via upload

---
+++ Tarea#10..py
@@ -0,0 +1,17 @@
+# Calcular la suma de los cuadrados de los primeros 
+# 100 enteros y escribir el resultado.
+
+class calcular:
+    def __init__(self):
+        pass
+    def variable(self):
+        print(" ")
+        i=1
+        suma=0
+        x=range(100)
+        for i in x:
+            suma=suma+i*i   
+            print("suma:",suma)
+        print(" ")
+    variable(" ")       
+         
