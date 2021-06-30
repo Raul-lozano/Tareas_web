8f14815ae8d8c88270c059248cee3249f98f6fe3
Raul-lozano <86394164+Raul-lozano@users.noreply.github.com>
2021-06-30 00:14:48 -0500

    Add files via upload

---
+++ Tarea#9.py
@@ -0,0 +1,21 @@
+#Diseñar un algoritmo tal que dados como datos dos variables de tipo entero, obtenga el resultado de la siguiente función:
+
+class Ejercicio9:
+    def _init_ (self):
+        pass
+    def variables (self):
+        NUME= int(input("Primer variable:"))
+        VA= int(input("Segunda variable "))
+        NUME+VA 
+        print(" ")
+        if NUME == 1: 
+            Resp = 100*VA
+        elif NUME ==2:
+            Resp= pow (100,VA)
+        elif NUME ==3:
+            Resp= 100/VA
+        else:
+            Resp=NUME+VA  
+            print("siguiente variable es:", Resp)
+        print("")
+    variables(" ")
