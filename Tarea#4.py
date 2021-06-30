44a603b682cb488a331b1c00b41b6253d07e3c7f
Raul-lozano <86394164+Raul-lozano@users.noreply.github.com>
2021-06-30 00:02:05 -0500

    Add files via upload

---
+++ Tarea#4.py
@@ -0,0 +1,15 @@
+#Construir un algoritmo tal, que dado como dato la calificación de un alumno en un examen.
+
+class dato:
+    def init(self):
+        pass
+    def Alumno(self):
+        print("")
+        cal=float(input("Ingrese su nota:"))
+        if cal >= 7 :
+            print()
+            print("Exelente")
+        if cal<7:
+            print("aceptable")
+            print()
+    Alumno(" ")
