6c6bb285a731509719e4c26109289f953f54cfc0
Raul-lozano <86394164+Raul-lozano@users.noreply.github.com>
2021-06-30 00:03:35 -0500

    Add files via upload

---
+++ Tarea#13.py
@@ -0,0 +1,20 @@
+# Diseñe un pseudocódigo para calcular la suma y producto de 
+# N números enteros, utilizando un bucle controlado por centinela.
+ 
+class Calcular:
+    def __init__(self):
+         pass
+    def variable(self):
+        print("")
+        produc=1
+        suma=0
+        numero=int(input("Ingresar un numero entero"))
+        print("")
+        while numero != -1:
+             numero=int(input("Ingresar un numero: "))
+             suma=suma+numero
+             prod=prod*numero
+             print(" ")   
+        print("""Total de la suma es:""",suma,"""Total del producto es: """,prod)
+    print(" ")
+    variable(" ")
