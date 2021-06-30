05781b5c1796064e76114a40dc7ab96beb24e04d
Raul-lozano <86394164+Raul-lozano@users.noreply.github.com>
2021-06-30 00:19:26 -0500

    Add files via upload

---
+++ Tarea#16.py
@@ -0,0 +1,19 @@
+#Calcular el factorial de N números enteros leídos de teclado.El problema consistirá en realizar una estructura de N iteraciones aplicando el factorial de un número.
+
+class Ejercicio16 :
+    def _init_(self):
+        pass
+    def  Factorial(self):
+        print("")
+        l = 1
+        n = int (input("ingrese un numero para la serie:"))
+        print("")
+        s = 5
+        ser = 0
+        for a in range ( l , n + 1 ):
+            s = s + 5
+            ser = ser + s
+        print ( "la suma de la serie es:" , ser )
+
+        print("")
+    Factorial("")
