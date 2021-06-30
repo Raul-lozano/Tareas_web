86ab028972e92d864fe6fff435603cd64197002e
Raul-lozano <86394164+Raul-lozano@users.noreply.github.com>
2021-06-30 00:03:04 -0500

    Add files via upload

---
+++ Tarea#11.py
@@ -0,0 +1,17 @@
+# Elabore pseudocódigo para el caso en que se desean 
+# escribir los números del 1 al 100.
+class Números:
+    def __init__(self):
+        pass
+    def variable(self):
+        print("")
+        print("ingrese los numeros hasta el 100")
+        i=1
+        for i in range(1,101):
+          print(i)
+        print("")
+    variable("")        
+        
+        
+
+    
