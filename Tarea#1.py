f46def714ca07a43a7984b2b96eb0eb3d883aabb
Raul-lozano <86394164+Raul-lozano@users.noreply.github.com>
2021-06-29 23:57:10 -0500

    Add files via upload

---
+++ Tarea#1.py
@@ -0,0 +1,16 @@
+# En ejemplos anteriores, diseñamos un pseudocódigo para encontrar la 
+# superficie de un círculo para un radio cualquiera.
+# El Flujograma que representa a dicho ejemplo es el siguiente:
+
+class ejercicio:
+    def __init__(self):
+        pass
+    def círculo(self):
+        print("")
+        cal=float(input("Ingrese su nota:"))
+        if cal >= 7 :
+            print()
+            print("Exelente estas aceptado")
+        print()
+    círculo("")
+ 
